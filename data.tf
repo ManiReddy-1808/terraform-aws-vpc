4011e21c111a1c9296b3724f25e08012971cdafe
ManiReddy-1808 <manireddy18082002@gmail.com>
2026-03-09 20:40:48 +0530

    Added new code

--- data.tf
+++ data.tf
@@ -5,3 +5,12 @@ data "aws_availability_zones" "availabile" {
 data "aws_vpc" "default" {
   default = true
 }
+
+ # Created automatically when we create VPC. Get all route tables of default VPC. 
+data "aws_route_tables" "default" {
+  vpc_id = data.aws_vpc.default.id
+  filter {
+    name   = "association.main" # Get only main route table of default VPC
+    values = ["true"]
+  }
+}
--- peering.tf
+++ peering.tf
@@ -23,3 +23,20 @@ resource "aws_vpc_peering_connection" "default" {
     }
   )
 }
+
+# Below is route from roboshop-dev to default VPC. 
+# route created in public route table of roboshop-dev VPC.
+resource "aws_route" "public_peering" { 
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default.id
+}
+
+# Below is route from default VPC to roboshop-dev. route created in main route table of default VPC.
+resource "aws_route" "default_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id = data.aws_route_tables.default.id
+  destination_cidr_block = var.vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.default.id
+}
