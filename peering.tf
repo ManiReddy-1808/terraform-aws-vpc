438db93126fccbe2c2ebbff1ce31be5278d7c7fa
ManiReddy-1808 <manireddy18082002@gmail.com>
2026-03-09 20:44:37 +0530

    Added new code

--- peering.tf
+++ peering.tf
@@ -30,7 +30,7 @@ resource "aws_route" "public_peering" {
   count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.default.id
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id # --> to get 1st peering connection ID 
 }
 
 # route in private route table of roboshop-dev VPC to default VPC.
@@ -38,7 +38,7 @@ resource "aws_route" "private_peering" {
   count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.private.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.default.id
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
 
 # route in database route table of roboshop-dev VPC to default VPC.
@@ -46,7 +46,7 @@ resource "aws_route" "database_peering" {
   count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.database.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.default.id
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
 
 # Below is route from default VPC to roboshop-dev. route created in main route table of default VPC.
@@ -54,5 +54,5 @@ resource "aws_route" "default_peering" {
   count = var.is_peering_required ? 1 : 0
   route_table_id = data.aws_route_tables.default.id
   destination_cidr_block = var.vpc_cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.default.id
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
